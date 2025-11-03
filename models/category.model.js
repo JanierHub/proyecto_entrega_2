9e031b1ccadea1710f992c6e7f8319a136dde2bd
Janier <Janierf.quinonesq@konradlorenz.edu.co>
2025-11-02 22:28:34 -0500

    Configuración archivo movido

--- models/category.model.js
+++ models/category.model.js
@@ -1,3 +1,4 @@
+// /models/category.model.js
 const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema({
@@ -6,15 +7,3 @@ const categorySchema = new mongoose.Schema({
 }, { timestamps: true });
 
 module.exports = mongoose.model("Category", categorySchema);
-const mongoose = require("mongoose");
-
-const postSchema = new mongoose.Schema({
-  titulo: { type: String, required: true },
-  contenido: { type: String, required: true },
-  autor: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  categoria: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
-  fecha: { type: Date, default: Date.now },
-  publicado: { type: Boolean, default: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model("Post", postSchema);
